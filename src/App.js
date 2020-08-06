0f3b2ad7d1179044d3912f2fa9356d588b5b12be
Gayan Madusanka <gayan.virajs@gmail.com>
2020-08-07 01:31:35 +0530

    Recipe JS added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Recipe from "./Recipe";
 
 const App = () => {
   const APP_ID = "1171c2b3";
   const APP_KEY = "1cee62e11cfd2b85dccba39f00fca2af";
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">
-          Search
+          Click Me
         </button>
       </form>
+      {recipes.map((recipe) => (
+        <Recipe />
+      ))}
+      ;
     </div>
   );
 };
